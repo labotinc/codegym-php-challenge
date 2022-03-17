1459d77bda28d40ae1112283afff4e501169c939
nagata-labot <nagata@labot.inc>
2022-03-17 06:09:06 +0000

    fix index.php

--- html/index.php
+++ html/index.php
@@ -71,7 +71,7 @@ $tweet_count = count($tweets);
           <p class="card-title"><b><?= "{$t['id']}" ?></b> <?= "{$t['name']}" ?> <small><?= "{$t['updated_at']}" ?></small></p>
           <p class="card-text"><?= "{$t['text']}" ?></p>
           <!--返信課題はここから修正しましょう。-->
-          <p>[返信する] [返信元のメッセージ]</p>
+          <!--<p>[返信する] [返信元のメッセージ]</p>-->
           <!--返信課題はここまで修正しましょう。-->
         </div>
       </div>
