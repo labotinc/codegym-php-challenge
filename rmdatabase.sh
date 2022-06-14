24a85bf42235ada12f6a782fee0f9eb994ee7a6f
nagata-labot <nagata@labot.inc>
2022-06-14 10:17:49 +0000

    fix mkenv.sh rmdatabase.sh url.sh

--- rmdatabase.sh
+++ rmdatabase.sh
@@ -1,7 +1,6 @@
 #! /usr/bin/sh
 
-ENV_DIR_PATH=~/environment
-REPOSITORY_DIR_PATH=${ENV_DIR_PATH}/codegym-php-challenge
+REPOSITORY_DIR_PATH=${PWD}
 DB_DATA_DIR_PATH=${REPOSITORY_DIR_PATH}/docker/db/var_lib_mysql
 
 if [ -e "${DB_DATA_DIR_PATH}" ]; then
